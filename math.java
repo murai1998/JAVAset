4491a08ea3fb83d8d050bbe141e37967718581ad
Hanna Murai <57275209+murai1998@users.noreply.github.com>
2020-10-13 10:58:06 -0500

    Create math.java

---
+++ math.java
@@ -0,0 +1,19 @@
+package program;
+
+public class mathOperat {
+public static void main(String[] args) {
+	int x = 5;
+	int y = 3;
+	int z;
+	z = x + y;
+	System.out.println(z);
+	System.out.println(x % y);
+	
+	float f = 3.5f;
+	int i = 4;
+	int res = (int)f + i;
+	double res2 = f + i;
+	System.out.println(res);
+	System.out.println(res2);
+}
+}
