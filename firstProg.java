ddc045c41f766c84fbd5374519dc3cd6800139c2
Hanna Murai <57275209+murai1998@users.noreply.github.com>
2020-10-13 10:31:56 -0500

    Create firstProg.java

---
+++ firstProg.java
@@ -0,0 +1,23 @@
+package program;
+
+public class helloWorld {
+public static void main(String[] args) {
+	System.out.println("Hello World");
+	
+	 boolean on = true;
+     System.out.println(on);
+     
+     int i = 20;
+     System.out.println(i);
+     
+     short s = 16;
+     System.out.println(s);
+     
+     float f = 3223.42334f;
+     System.out.println(f);
+     
+     
+     long l = 322342334999L;
+     System.out.println(l);
+}
+}
